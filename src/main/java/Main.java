2b201bbf1d3c129c78073b5ce2a8906eb5a5fd14
Deepthi Ganji <115901667+deepthireddyg@users.noreply.github.com>
2022-11-01 19:04:43 -0400

    finished the Guessing game

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,30 @@
+import java.util.Scanner;
+//import java.util.Scanner;
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+
+        int secretNumber = 50;
+
+        System.out.println("Secret number is " + secretNumber);
+
+        Scanner keyboard = new Scanner(System.in);
+
+        int guess = 100;
+
+        System.out.print("Enter a guess: ");
+
+        guess = keyboard.nextInt();
+
+        if (guess == secretNumber)
+            System.out.println("your guess is correct.Congratulations");
+        else if (guess < secretNumber)
+            System.out.println("your guess is Too small");
+        else if (guess > secretNumber)
+            System.out.println("your guess is Too Large");
     }
 }
+
